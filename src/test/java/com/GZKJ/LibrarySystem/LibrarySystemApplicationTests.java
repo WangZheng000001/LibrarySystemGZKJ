a7e6b2dbbf15466d14e5bff7e0d0d968c600e450
WangZheng000001 <wangzheng000001@gmail.com>
2022-12-14 15:26:02 +0900

    This is the first commit to LibrarySystem for practicing as GZKJ

---
+++ src/test/java/com/GZKJ/LibrarySystem/LibrarySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GZKJ.LibrarySystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
